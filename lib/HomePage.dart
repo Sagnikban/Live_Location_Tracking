698bce6f6bc2a785c075e40f25d9c6d79306e148
Sagnikban <banerjeesagnik29@gmail.com>
2022-03-24 19:53:02 +0530

    Welcome Page and Sign Up Page UI

---
+++ lib/HomePage.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:location_tracking_app/SignUp.dart';
+class HomePageWidget extends StatefulWidget {
+  const HomePageWidget({Key? key}) : super(key: key);
+  @override
+  _HomePageWidgetState createState() => _HomePageWidgetState();
+}
+
+class _HomePageWidgetState extends State<HomePageWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(children: <Widget>[
+            SizedBox(height: 80),
+            Container(
+                height: 400,
+                width: 400,
+                child: Image(image: NetworkImage('https://res.cloudinary.com/dddvpe70r/image/upload/v1648128506/3259200_dkbtqn.png'))),
+            SizedBox(height: 100),
+            FlatButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignUp()),
+                  );
+                },
+                child: Container(
+                  height: 60,
+                  width: 500,
+                  color: Colors.greenAccent,
+                  child: Center(
+                      child: Text(
+                    "Let's Get Started",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  )),
+                ))
+          ]),
+        ),
+      ),
+    );
+  }
+}
